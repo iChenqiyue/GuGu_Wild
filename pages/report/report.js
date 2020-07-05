b135d47a30f9bfc4d5d8ae02c4d1e00b56d735ed
陈七月 <947252923@qq.com>
2020-07-05 22:13:26 +0800

    1.0.1
    添加report

---
+++ pages/report/report.js
@@ -0,0 +1,85 @@
+// pages/report/report.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    steps: [
+      {
+        text: '步骤一',
+        desc: '选择举报类别',
+        inactiveIcon: 'info-o',
+        activeIcon: 'success',
+      },
+      {
+        text: '步骤二',
+        desc: '填写表单',
+        inactiveIcon: 'records',
+        activeIcon: 'success',
+      },
+      {
+        text: '步骤三',
+        desc: '完成举报',
+        inactiveIcon: 'star-o',
+        activeIcon: 'success',
+      },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
